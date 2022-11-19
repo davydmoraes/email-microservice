b48965746545c5e6b80cc0af32dfb0390fc7a36a
Davyd Moraes <davyd.moraes@live.com>
2022-11-19 17:43:31 -0300

    - application is now ready to work with asynchronous messages

--- email/src/main/java/com/davyd/email/repositories/EmailRepository.java
+++ email/src/main/java/com/davyd/email/repositories/EmailRepository.java
@@ -1,9 +1,11 @@
 package com.davyd.email.repositories;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.davyd.email.models.EmailModel;
 
-public interface EmailRepository extends JpaRepository<EmailModel, Long> {
+public interface EmailRepository extends JpaRepository<EmailModel, UUID> {
 
 }
